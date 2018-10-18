fec9c211bad7c2b61b6720e88b791a4ce573724d
Shijil <shijil@gmail.com>
2018-10-18 19:36:38 +0530

    Added validation of ids

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 // mongoose variable assigned from the object returned from mongoose.js appendFile
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -29,6 +29,24 @@ app.get ('/todos',(req, res)=>{
   },(e)=>{
     res.status(400).send(e)
   });
+});
+
+app.get('/todos/:id',(req, res)=>{
+  // res.send(req.params);
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)){
+    return res.status(404).send()
+  }
+  Todo.findById(id).then((todo)=>{
+    if (!todo){
+      return res.status(404).send()
+    }
+    res.send({todo})
+  }).catch((e)=>{
+    return res.status(404).send();
+  });
+
+
 });
 
 app.listen(3000, () =>{
